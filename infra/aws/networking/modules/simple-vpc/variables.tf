060ecd3da7d1f3990ad9e1755649d852fbec0a8e
acanave <cape915@gmail.com>
2025-02-19 12:48:13 -0600

    fixed a missing reference for availability zones in subnets.tf

--- infra/aws/networking/modules/simple-vpc/variables.tf
+++ infra/aws/networking/modules/simple-vpc/variables.tf
@@ -9,7 +9,7 @@ variable "cidr_block" {
   default = "10.0.0.0/16"
 }
 
-variable "azs" {
+variable "availability_zones" {
   description = "List of availability zones"
   type        = list(string)
   default = ["us-east-1a", "us-east-1b", "us-east-1c"]
